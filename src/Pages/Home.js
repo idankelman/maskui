e726ef06e2966ff4af46b849921af2e5a75426d0
idankelman <kelmanidan@gmail.com>
2022-03-09 21:37:19 +0200

    Created Simple Template for Home page , NewSession Page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,36 @@
 
 function Home() {
     return (
+
+
+
         <div className ="Home">
+
+            <div className="Title">
+                <h1>Welcome to MaskDetect</h1>
+                <h2>A simple app to detect masks</h2>
+            </div>
             {/* <NavBar /> */}
-            <div>This Is Home </div>
+            <div className="Wrapper">
+                <div className="Left">
+                    <h1>Left</h1>
+                </div>
+                <div className="Right">
+                    <h1>Right</h1>
+                    <div className="Top">
+                        <div className="Container">
+                            <h1>Top</h1>
+                        </div>
+                    </div>
+                    <div className="Center">
+                        <h1>Center</h1>
+                    </div>
+
+                    <div className="Bottom">
+                        <h1>Bottom</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Pages/NewSession.js
+++ src/Pages/NewSession.js
@@ -1,4 +1,46 @@
+
+
 function NewSession(){
-    return <div>This Is Home </div>;
+    return (
+
+        <div className ="NewSession">
+
+        <div className="Title">
+            <h1>New Session</h1>
+            <div class = "Reset">
+                <button>Reset</button>
+                <h2>user</h2>
+            </div>
+            
+        </div>
+        <div className="Wrapper">
+            <div className="Left">
+                <h1>Left</h1>
+                <button>Start seesion</button>
+                <button>Choose Threshold</button>
+            </div>
+            <div className="Right">
+                <h1>Right</h1>
+                <div className="Top">
+                    <div className="Container">
+                        <h1>Top</h1>
+                        <div className="Stream">
+                            <h1>Stream</h1>
+                            <img src= "https://d3lkc3n5th01x7.cloudfront.net/wp-content/uploads/2020/04/01023335/MaskDetection_Banner-1-1.png" alt ="mask" />
+                        </div>
+                    </div>
+                </div>
+                <div className="Center">
+                    <h1>Center</h1>
+                </div>
+
+                <div className="Bottom">
+                    <h1>Bottom</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    );
     }
     export default NewSession;
