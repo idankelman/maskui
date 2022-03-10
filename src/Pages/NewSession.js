e83ec999c5bec252ae59d55a3b1b37880dc99ae3
idan kelman <kelmanidan@gmail.com>
2022-03-10 02:07:23 +0200

    added View people on the bottom , arranged the NewSession correctly

--- src/Pages/NewSession.js
+++ src/Pages/NewSession.js
@@ -1,45 +1,69 @@
 
 
-function NewSession(){
-    return (
-        <div className ="NewSession">
+import logo from "../Assets/mask.png";
 
-        <div className="Title">
-            <h1>New Session</h1>
-            <div className = "Reset">
-                <button><h1>Reset</h1></button>
-                <h2>user</h2>
+
+function NewSession() {
+    return (
+        <div className="NewSession">
+
+            <div className="Title">
+                New Session
+                <div className="Search">
+                    <div className="InputBox">
+                        <input type = "text"/>
+                    </div>
+                    <button>Search</button>
+                </div>
+                <div className="Logo">
+                    <img src={logo} />
+                </div>
             </div>
             
-        </div>
-        <div className="Wrapper">
-            <div className="Left">
-                <h1>Left</h1>
-                <button>Start seesion</button>
-                <button>Choose Threshold</button>
-            </div>
-            <div className="Right">
-                <h1>Right</h1>
-                <div className="Top">
-                    <div className="Container">
-                        <h1>Top</h1>
-                        <div className="Stream">
-                            <h1>Stream</h1>
-                            <img src= "https://d3lkc3n5th01x7.cloudfront.net/wp-content/uploads/2020/04/01023335/MaskDetection_Banner-1-1.png" alt ="mask" />
+            <div className="Wrapper">
+                <div className="Left">
+                    {/* <h1>Left</h1> */}
+
+                    <div className="Start">
+                        <button><h2>Start seesion</h2></button>
+                        <button><h2>Choose Threshold</h2></button>
+                    </div>
+
+                    <div className="Anlyze">
+                        <button><h2>Statistics</h2></button>
+                    </div>
+                </div>
+                <div className="Right">
+                    {/* <h1>Right</h1> */}
+                    <div className="Top">
+                        <div className="Container">
+                            Top
+                            <div className="Stream">
+                                {/* <h1>Stream</h1> */}
+                                <img src="https://d3lkc3n5th01x7.cloudfront.net/wp-content/uploads/2020/04/01023335/MaskDetection_Banner-1-1.png" alt="mask" />
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="Center">
-                    <h1>Center</h1>
-                </div>
 
-                <div className="Bottom">
-                    <h1>Bottom</h1>
-                </div>
+            </div>
+            <div className="Bottom">
+                <div className="Person"></div>
+                <div className="Person"></div>
+                <div className="Person"></div>
+                <div className="Person"></div>
+                <div className="Person"></div>
+                <div className="Person"></div>
+                <div className="Person"></div>
+                <div className="Person"></div>
+                <div className="Person"></div>
+                <div className="Person"></div>
+                <div className="Person"></div>
+                <div className="Person"></div>
+                {/* <h1>Bottom</h1> */}
             </div>
         </div>
-    </div>
 
     );
-    }
-    export default NewSession;
+}
+export default NewSession;
