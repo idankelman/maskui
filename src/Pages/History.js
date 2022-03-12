359870b62b1368c4fd42957c950b3979d6294391
idankelman <kelmanidan@gmail.com>
2022-03-12 12:42:12 +0200

    basic history page with calender

--- src/Pages/History.js
+++ src/Pages/History.js
@@ -1,10 +1,16 @@
 import Title from "../Components/Title";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 function History() {
-    return (
+  const [value, onChange] = useState(new Date());
+
+  return (
     <div className="History">
-        <Title title="History"/>
+      <Title title="History" />
+      <Calendar onChange={onChange} value={value} />
     </div>
-    );
+  );
 }
 export default History;
