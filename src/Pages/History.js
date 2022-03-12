98a51d99944009e4e527ab41aeefc2797ed641ee
idankelman <kelmanidan@gmail.com>
2022-03-12 18:56:55 +0200

    added custom styling , added calendar to the history page . isnt finished yet tho

--- src/Pages/History.js
+++ src/Pages/History.js
@@ -1,15 +1,17 @@
 import Title from "../Components/Title";
 import React, { useState } from "react";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+// import "react-calendar/dist/Calendar.css";
 
 function History() {
   const [value, onChange] = useState(new Date());
 
   return (
     <div className="History">
-      <Title title="History" />
-      <Calendar onChange={onChange} value={value} />
+        <Title title="History" />
+        <div className="Calendar">
+            <Calendar onChange={onChange} value={value} />
+        </div>
     </div>
   );
 }
