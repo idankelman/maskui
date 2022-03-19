c9a9c5e128db87aa4e7dc7e92855af667dc7102b
idan kelman <80175752+idankelman@users.noreply.github.com>
2022-03-19 14:14:36 +0200

    added functionality options to the calendar+styling

--- src/Pages/History.js
+++ src/Pages/History.js
@@ -4,14 +4,20 @@ import Calendar from "react-calendar";
 // import "react-calendar/dist/Calendar.css";
 
 function History() {
-  const [value, onChange] = useState(new Date());
+  const [value, setDate] = useState(new Date());
+  const onChange =
+    date => {
+      setDate(date);
+      console.log(date);
+    };
+
 
   return (
     <div className="History">
-        <Title title="History" />
-        <div className="Calendar">
-            <Calendar onChange={onChange} value={value} />
-        </div>
+      <Title title="History" />
+      <div className="Calendar">
+        <Calendar onChange={onChange} value={value} />
+      </div>
     </div>
   );
 }
