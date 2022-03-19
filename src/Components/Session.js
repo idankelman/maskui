43e4aab760daaa5c3cba21d5b75e66b95a595efe
idankelman <kelmanidan@gmail.com>
2022-03-19 16:44:30 +0200

    Showing Sessions + style

--- src/Components/Session.js
+++ src/Components/Session.js
@@ -2,17 +2,21 @@ import React from "react";
 
 function Session(params) {
   return (
-
     <div className="Session">
-      <div className="Date">
+      <div className="Session_Title">
         <h2>{params.date}</h2>
         <h2>{params.time}</h2>
       </div>
-      <div className="Duration">
-        <h2>{params.duration}</h2>
+      <div className="Session_Content">
+        <div className="details">
+          <h2>Content of the duration</h2>
+          <h2>duration : {params.duration}</h2>
+        </div>
+        <div className="Actions">
+          <button>go</button>
+        </div>
       </div>
     </div>
   );
-
 }
 export default Session;
