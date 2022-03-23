3ac4567be70e989ce2643ce302d3db3edfc0920e
idan kelman <80175752+idankelman@users.noreply.github.com>
2022-03-23 07:25:12 +0200

    utilize the modal inside the title

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,13 +1,44 @@
 
+import { useRef, useState } from 'react';
 import logo from '../Assets/user.png';
+import Modal from "./Modal";
+
 
 function Title(params) {
 
+
+    const [modal, setModal] = useState(false);
+    const SearchModal = useRef(null);
+    const DUMMY_DATA = [
+        {
+            id: 1,
+            image: logo,
+            name: "Diana Doe",
+        },
+        {
+
+            id: 2,
+            image: logo,
+            name: "John Doe",
+        },
+        {
+            id: 3,
+            image: logo,
+            name: "Omer Doe",
+        },
+    ];
 
     //=========================================================
     //     Fuctions
     //=========================================================
 
+    function filterProps() {
+        setModal(!modal);
+    }
+
+    function toggleModal(){
+        setModal(!modal);
+    }
 
     //=========================================================
     //     Render
@@ -22,11 +53,14 @@ function Title(params) {
                 <div className="InputBox">
                     <input type="text" />
                 </div>
-                <button>Search</button>
+                <button onClick= {toggleModal}>Search</button>
             </div>
             <div className="Logo">
-                <img src={logo} alt = "logo" />
+                <img src={logo} alt="logo" />
             </div>
+            {modal ?
+                <Modal ref={SearchModal} onClick={toggleModal} toggle ={toggleModal} data = {DUMMY_DATA}></Modal> :
+                null}
         </div>
     );
 
