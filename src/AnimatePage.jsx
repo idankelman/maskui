03cd9bb14f6f72bd6f1af9864dab5c2a6c3e1290
idan kelman <80175752+idankelman@users.noreply.github.com>
2022-03-24 15:11:24 +0200

    basic animations file

---
+++ src/AnimatePage.jsx
@@ -0,0 +1,21 @@
+
+
+import { motion } from 'framer-motion';
+
+const animations = {
+    inital : {},
+    animate : {},
+    exit : {}
+}
+
+const AnimatePage = ({children}) => {
+    return (
+        <div>
+
+            <motion.div >
+
+            </motion.div>
+        </div>
+    );
+}
+export default AnimatePage;
