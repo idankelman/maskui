1ad6307fe1f08724e98700eb7481c9bcffffcbfd
idan kelman <80175752+idankelman@users.noreply.github.com>
2022-04-03 07:26:40 +0300

    functionality for testing the base64 is working on the contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,8 +1,50 @@
 import Title from "../Components/Title";
+import { useState } from "react";
+import { convertBase64 } from "../Services/Base64";
+
+
+//==========================================================================
 
 function Contact() {
-    return (<div className = "Contact">
-        <Title title="Contact"/>
+
+    const [baseImage, setBaseImage] = useState("");
+
+    const uploadImage = async (e) => {
+        const file = e.target.files[0];
+        const base64 = await convertBase64(file);
+        setBaseImage(base64);
+    };
+
+    // const convertBase64 = (file) => {
+    //     return new Promise((resolve, reject) => {
+    //         const fileReader = new FileReader();
+    //         fileReader.readAsDataURL(file);
+
+    //         fileReader.onload = () => {
+    //             resolve(fileReader.result);
+    //         };
+
+    //         fileReader.onerror = (error) => {
+    //             reject(error);
+    //         };
+    //     });
+    // };
+
+
+    return (<div className="Contact">
+        <Title title="Contact" />
+        <div className="Contact-content">
+            {/* <input type = "text"></input> */}
+
+            <input
+                type="file"
+                onChange={(e) => {
+                    uploadImage(e);
+                }}
+            />
+            <br></br>
+            <img src={baseImage} height="200px" />
+        </div>
     </div>);
 }
 export default Contact;
--- src/Services/Base64.js
+++ src/Services/Base64.js
@@ -1,25 +1,40 @@
-import {base64encoder, base64decoder ,Buffer} from "base64image";
+// // import {base64encoder, base64decoder ,Buffer} from "base64image";
+// const base64 = require("base64image");
 
 
-export const Encode = (url) => {
-    // var options = {string: true};
-    options = {localFile: true, string: true};
-    base64encoder(url, options, function (err, image) {
-        if (err) {
-            console.log(err);
-        }
-        console.log(image);
-    });
-}
+// export const Encode = (url) => {
+//     // var options = {string: true};
+//     let options = {localFile: true, string: true};
+//     base64.base64encoder(url, options, function (err, image) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log(image);
+//     });
+// }
 
 
-export const Decode = (base64) => {
-    var options = {filename: 'test', filePath: './tmp/uploads'};
-    var imageData = new Buffer(base64, 'base64');
+// export const Decode = (base64) => {
+//     var options = {filename: 'test', filePath: './tmp/uploads'};
+//     var imageData = new base64.Buffer(base64, 'base64');
     
-    base64decoder(imageData, options, function (err, saved) {
-        if (err) { console.log(err); }  
-        console.log(saved);    
-        return saved;
-    });  
-}
+//     base64.base64decoder(imageData, options, function (err, saved) {
+//         if (err) { console.log(err); }  
+//         console.log(saved);    
+//         return saved;
+//     });  
+// }
+export const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+
+        fileReader.onload = () => {
+            resolve(fileReader.result);
+        };
+
+        fileReader.onerror = (error) => {
+            reject(error);
+        };
+    });
+};
