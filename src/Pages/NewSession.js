12d9e3f4e27f81bde4684b0e08a2ac2b7f549dad
idan kelman <80175752+idankelman@users.noreply.github.com>
2022-04-27 20:52:40 +0300

    update

--- src/Pages/NewSession.js
+++ src/Pages/NewSession.js
@@ -1,10 +1,10 @@
 
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import Person from "../Components/Person";
 import Title from "../Components/Title";
-import { init_ws, send_message } from '../Services/Websocket'
+import { init_ws, send_message,close_ws } from '../Services/Websocket'
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -14,38 +14,25 @@ toast.configure()
 
 function NewSession() {
 
-
-
     //==========================================================================
     //                          Defninng Reference Variables
     //==========================================================================
 
     const [isLoading, setLoading] = useState(true);
-    const [TransLog, UpdateTransLog] = useState("");
+    const [capture, setCap] = useState('https://static.videezy.com/system/resources/previews/000/014/051/original/pixel_loading_bar.mp4');
+    const [TransLog, UpdateTransLog] = useState('');
     const [People,UpdatePeople] = useState([]);
     const ToastMessage="Incomming data about the latest transactions will be displayed here";
 
 
     let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiYkwrNE5IVDlHdWx2QWU4clhVQmVKNFJaNG02UHd0bmUxRDBQcW9sdXVqa21VOG9vR1lDTkdSalYzUW5kVUxQRVgyVTdLN2dnZGIzVmU4ZlFhclJnT3NYSlQrSUhLL05kUWlUUVRRR0U1WjVCbkVUekU2SFNOa0NFQ3NDVUhoWjE4RE1yQ1hzVTFXK3hjUXI1VzlIM3FkcGxWQWh2TEpTZGlKNGxMZm41NWN3K3g0SkRSbERWRUloTnRYbWdVNGRBYnY4WWJCaW9nazFrTWltU0FHWmluTFF5TXFVRnpHU09zbURXaGgxZEM3ZVQrUVR6UExqYmRaVndUUFE3REpadHN5Y2lzS1ZGRXQveE8wQWk1Vm01Yy9MeGQ1emRFOFYzMzFHb1ZsT0l1YzFsa08yU0JjTTEycGQ1b3NUY0JHVzJNdTF3cmRqalpCMFJXRDl5cmV6Z0ZEUm9FVWw2U3dNL014MmhsT2wyZGlYdWsrZmxUdVNLUE1Xd1ZOa3dNd0pVUVJ5VU1JU0l6bHl4ZERvaXFVa3FBM1habHpBVC9FVC9TMGVydzhySjBsOVErbmFISit6clByb1hzTWRwTHJoanZMcXVuV1NjS2NxYUxyRHNmSEt1SnJmTktWVWluQVVSdGdDYnNOMGNSWkJRcGx1K1dvMWp4VnFEM3Yrb0pNbkova0Q1RDl4eVdjc1RRbDVGSkNpYWNBPT0iLCJpYXQiOjE2NDQxNDk2NDksImV4cCI6MTY0NDIzNjA0OX0.eC8JyBomUeV_PDzBU6WlN5MNBjXEXZjSZ8bkQkewY6M";
-    let message = {
-        "type": "subscription",
-        "id": "bf5d15d0-415f-11ec-b255-ad01e0712738",
-        "data": {
-            "products": ["BTC-USD"],
-            "quantity": "3.00000000",
-            "level": true,
-            "high": true,
-            "low": true,
-            "change_daily": true
-        }
-    };
-    message = "_____";
+    let message = "_____";
 
     const DUMMY_DATA = {
         "item": {
+            "capture": 'http://marina.art-net.co.il:120/mjpg/video.mjpg',
             "name": "blue dress",
             "details": "graphic print, Logo.",
-            "composition": "Composition: 94% Cotton, 6% Elastam.",
             "modelDetails": [
                 "Modeal wearing a size M",
                 "Measures: 86 - 60 - 90",
@@ -113,7 +100,7 @@ function NewSession() {
 
                     let data = DUMMY_DATA;
                     setPeople(data);
-                 
+                    setCap(data.item.capture);
 
                     //==================================  Toast   ================================
 
@@ -155,6 +142,9 @@ function NewSession() {
     }
 
 
+    
+
+
     return (
         <div className="NewSession">
             <Title title="New" />
@@ -181,7 +171,7 @@ function NewSession() {
                             <div className="Stream">
                                 {/* <h1>Stream</h1> */}
                                 {/* <img src={'http://localhost:5000/video'} alt="mask" /> */}
-                                <img src={'http://marina.art-net.co.il:120/mjpg/video.mjpg'} alt="mask" />
+                                <img src={capture} alt="mask" />
                             </div>
                         </div>
                     </div>
