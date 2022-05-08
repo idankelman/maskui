5a9f463675296686e418db68f6e94df95065abd1
idan kelman <80175752+idankelman@users.noreply.github.com>
2022-05-08 07:25:36 +0300

    starting basic raw dummy data implemination of statistics : showing total users and the avrg

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -7,27 +7,61 @@ import {
   Bar,
   Line,
   CartesianGrid,
-    LineChart,
-    RadarChart,
-    PolarGrid,
-    PolarAngleAxis,
-    PolarRadiusAxis,
-    Radar,
-    Legend,
-    PieChart,
-    Pie,
+  LineChart,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  Radar,
+  Legend,
+  PieChart,
+  Pie,
+  ZAxis
 } from "recharts";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Chart(params) {
+
+
   const [State, UpdateState] = useState("high");
   const [StateColor, UpdateColor] = useState("#8884d8");
 
+  const [avergae, UpdateAverage] = useState([
+    { mask: 2421.4285714285716,
+      no_mask: 1072.857142857143,
+      nose: 684.2857142857143
+    },
+    { mask: 2421.4285714285716,
+      no_mask: 1072.857142857143,
+      nose: 684.2857142857143
+    },,
+    { mask: 2421.4285714285716,
+      no_mask: 1072.857142857143,
+      nose: 684.2857142857143
+    }
+  ]
+  );
+
+
   const Pallet = ["#8884d8", "#82ca9d"];
   const States = ["high", "low"];
+  const classes = ["mask", "nose", "no_mask"];
+
+  const ClassPallet = ["#8884d8", "#82ca9d", "#ffc658"];
+
 
   let Val = "high";
   let Color = "#8884d8";
+
+
+
+  useEffect(() => {
+    // avgData();
+  }, []);
+
+
+
+
 
   function ChangeState() {
     console.log("StateChange");
@@ -44,52 +78,108 @@ function Chart(params) {
     // console.log(Pallet)
   }
 
+  function avgData() {
+
+    let data = params.total_data;
+    let avg = { mask: 0, nose: 0, no_mask: 0 };
+
+    for (let i = 0; i < data.people.length; i++) {
+      avg.mask += data.people[i].mask;
+      avg.nose += data.people[i].nose;
+      avg.no_mask += data.people[i].no_mask;
+    }
+
+
+    
+    avg.mask = parseInt(avg.mask / data.people.length);
+    avg.nose = parseInt(avg.nose / data.people.length);
+    avg.no_mask = parseInt(avg.no_mask / data.people.length);
+    UpdateAverage(avg);
+    console.log(avg);
+  }
+
   return (
     <div>
       <div className="ChartHolder" id="Charter">
+        <div className="ChartTitle">
+          <h2>People classification</h2>
+        </div>
         <button className="button1" text="test" onClick={ChangeState}>
           {State}
         </button>
         <ResponsiveContainer width="90%" height={500}>
-          <BarChart width="100%" height={300} data={params.data}>
-            <Bar dataKey={State} fill={StateColor} />
+          <BarChart width="100%" height={300} data={params.total_data.people}>
+            <Bar dataKey={'mask'} fill={ClassPallet[0]} />
+            <Bar dataKey={'no_mask'} fill={ClassPallet[1]} />
+            <Bar dataKey={'nose'} fill={ClassPallet[2]} />
             {/* <Bar dataKey="low" fill="#82ca9d" /> */}
             {/* <Bar dataKey="low" fill="#82ca9d" /> */}
             <XAxis dataKey="Time"></XAxis>
-            <YAxis dataKey={State} domain={params.data}></YAxis>
+            {/* <YAxis dataKey="Time"></YAxis> */}
+            <YAxis dataKey={'mask'} domain={params.total_data.people}></YAxis>
+            {/* <XAxis dataKey={'no_mask'} domain={params.total_data.people}></XAxis> */}
             <Tooltip></Tooltip>
           </BarChart>
         </ResponsiveContainer>
 
 
       </div>
-      <div className = "ChartHolder" >
-          
-        <LineChart width={500} height={300} data={params.data}>
-          <XAxis dataKey="high" />
+
+      <div className="ChartHolder" id="Charter">
+        <div className="ChartTitle">
+          <h2>avergae</h2>
+        </div>
+        <button className="button1" text="test" onClick={avgData}>
+          {State}
+        </button>
+        <ResponsiveContainer width="90%" height={500}>
+          <BarChart width="100%" height={300} data={avergae}>
+            <Bar dataKey={'mask'} fill={ClassPallet[0]} />
+            <Bar dataKey={'no_mask'} fill={ClassPallet[1]} />
+            <Bar dataKey={'nose'} fill={ClassPallet[2]} />
+            {/* <Bar dataKey="low" fill="#82ca9d" /> */}
+            {/* <Bar dataKey="low" fill="#82ca9d" /> */}
+            <XAxis dataKey="Time"></XAxis>
+            {/* <YAxis dataKey="Time"></YAxis> */}
+            <YAxis dataKey={'mask'} domain={avergae}></YAxis>
+            {/* <XAxis dataKey={'no_mask'} domain={params.total_data.people}></XAxis> */}
+            <Tooltip></Tooltip>
+          </BarChart>
+        </ResponsiveContainer>
+
+
+      </div>
+
+      <div className="ChartHolder" >
+
+        <LineChart width={500} height={300} data={params.total_data.people}>
+          <XAxis dataKey="mask" />
           <YAxis />
           <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-          <Line type="monotone" dataKey="high" stroke="#8884d8" />
-          <Line type="monotone" dataKey="low" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="mask" stroke={ClassPallet[0]} />
+          <Line type="monotone" dataKey="no_mask" stroke={ClassPallet[1]} />
+          <Line type="monotone" dataKey="nose" stroke={ClassPallet[2]} />
         </LineChart>
       </div>
 
-      <div className = "ChartHolder" >
+      <div className="ChartHolder" >
 
-        <RadarChart outerRadius={90} width={730} height={250} data={params.data}>
-            <PolarGrid />
-            <PolarAngleAxis dataKey="high" />
-            <PolarRadiusAxis angle={30} domain={[0, 150]} />
-            <Radar name="Mask" dataKey="high" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-            <Radar name="No_Mask" dataKey="low" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} />
-            <Legend />
+        <RadarChart outerRadius={90} width={730} height={250} data={params.total_data.people}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="mask" />
+          <PolarRadiusAxis angle={30} domain={[0, 150]} />
+          <Radar name="mask" dataKey="mask" stroke={ClassPallet[0]} fill={ClassPallet[0]} fillOpacity={0.6} />
+          <Radar name="no_mask" dataKey="no_mask" stroke={ClassPallet[1]} fill={ClassPallet[1]} fillOpacity={0.6} />
+          <Radar name="nose" dataKey="nose" stroke={ClassPallet[2]} fill={ClassPallet[2]} fillOpacity={0.6} />
+          <Legend />
         </RadarChart>
       </div>
-      
+
       <div className="ChartHolder">
         <PieChart width={730} height={250}>
-            <Pie data={params.data} dataKey="high" nameKey="high" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
-            <Pie data={params.data} dataKey="low" nameKey="low" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+          <Pie data={params.total_data.people} dataKey="mask" nameKey="mask" cx="50%" cy="50%" outerRadius={50} fill={ClassPallet[0]} />
+          <Pie data={params.total_data.people} dataKey="no_mask" nameKey="no_mask" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill={ClassPallet[1]} label />
+          {/* <Pie data={params.total_data.people} dataKey="nose" nameKey="nose" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill={ClassPallet[2]} label /> */}
         </PieChart>
       </div>
     </div>
--- src/Pages/Statistics.js
+++ src/Pages/Statistics.js
@@ -1,10 +1,14 @@
 import Title from "../Components/Title";
 import Chart from "../Components/Chart";
+import { useState } from "react";
 
 
 
 
 function About() {
+
+    const [Profiles,UpdateProfiles] = useState([]);
+
 
     const DATA = [
         {high:14002,low:300},
@@ -20,14 +24,65 @@ function About() {
         {high:24020,low:1300},
         {high:25020,low:1400},
     ];
-    const max_amount=110; 
 
+    const S_DATA = {
+        'classification':{
+            'mask':1000,
+            'nose':40,
+            'no_mask':500
+        },
+        people:
+        [
+            {
+                id:1,
+                'mask':1000,
+                'nose':40,
+                'no_mask':500
+            },
+            {
+                id:2,
+                'mask':5000,
+                'nose':40,
+                'no_mask':1000
+            },
+            {
+                id:3,
+                'mask':2000,
+                'nose':30,
+                'no_mask':1500
+            },
+            {
+                id:4,
+                'mask':150,
+                'nose':40,
+                'no_mask':1500
+            },
+            {
+                id:5,
+                'mask':6500,
+                'nose':10,
+                'no_mask':10
+            },{
+                id:6,
+                'mask':200,
+                'nose':4220,
+                'no_mask':1500
+            },
+            {
+                id:7,
+                'mask':2100,
+                'nose':410,
+                'no_mask':1500
+            }
+    ]}
+
+    const max_amount=110; 
 
 
     return (
     <div className="About">
         <Title title="About"/>
-        <Chart data={DATA} max_amount= {max_amount}/>
+        <Chart data={DATA} total_data = {S_DATA} max_amount= {max_amount}/>
     </div>
     );
 }
